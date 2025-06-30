59a9eaf10c889785e9eb9960a53c6133c1950f7f
tarsisms <tarsis.souza@ifal.edu.br>
2025-06-30 14:06:09 -0300

    Aula 08 - Navegação e Tela de Login - 30/06

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:aula_923/home_page.dart';
+import 'package:aula_923/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: HomePage()));
+  runApp(const MaterialApp(home: LoginPage()));
 }
