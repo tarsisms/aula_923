0ac0fdac4dd3cb907ff666129953a59e9c206ef8
tarsisms <tarsis.souza@ifal.edu.br>
2025-07-14 13:40:08 -0300

    Aula 14 - Listview.builder - 14/07

---
+++ lib/db/database.dart
@@ -0,0 +1,33 @@
+import 'package:aula_923/domain/propriedade.dart';
+
+class Database {
+  static List propriedades = [
+    Propriedade(
+      valor: '2,500',
+      avaliacao: 5.0,
+      datas: '12-31 Dec',
+      local: 'Maragogi, Brazil',
+      tipoDeHost: 'Professional Host',
+      urlImage:
+          'https://news.airbnb.com/wp-content/uploads/sites/4/2019/06/PJM020719Q202_Luxe_WanakaNZ_LivingRoom_0264-LightOn_R1.jpg?fit=3200%2C2133',
+    ),
+    Propriedade(
+      valor: '1,065',
+      avaliacao: 4.0,
+      datas: '18-23 Dec',
+      local: 'Harlingen, Netherlands',
+      tipoDeHost: 'Professional Host',
+      urlImage:
+          'https://news.airbnb.com/wp-content/uploads/sites/4/2019/06/PJM020719Q202_Luxe_WanakaNZ_LivingRoom_0264-LightOn_R1.jpg?fit=3200%2C2133',
+    ),
+    Propriedade(
+      valor: '2,225',
+      avaliacao: 4.80,
+      datas: '10-31 Dec',
+      local: 'Harlingen, Netherlands',
+      tipoDeHost: 'Professional Host',
+      urlImage:
+          'https://news.airbnb.com/wp-content/uploads/sites/4/2019/06/PJM020719Q202_Luxe_WanakaNZ_LivingRoom_0264-LightOn_R1.jpg?fit=3200%2C2133',
+    ),
+  ];
+}
---
+++ lib/domain/propriedade.dart
@@ -0,0 +1,17 @@
+class Propriedade {
+  String urlImage;
+  String local;
+  String tipoDeHost;
+  String datas;
+  String valor;
+  double avaliacao;
+
+  Propriedade({
+    required this.urlImage,
+    required this.local,
+    required this.avaliacao,
+    required this.tipoDeHost,
+    required this.datas,
+    required this.valor,
+  });
+}
