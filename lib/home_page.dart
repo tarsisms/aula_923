73c2f47ba584522391ed668fdb550b58413dab1e
tarsisms <tarsis.souza@ifal.edu.br>
2026-04-07 16:54:45 -0300

    Aula 05 - Interfaces Gráficas - 07/04

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
+import 'package:aula_923/explore_page.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,78 +10,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  List pages = [
+    ExplorePage(),
+    Center(child: Text('Pagina 1', style: TextStyle(fontSize: 32))),
+    Center(child: Text('Pagina 2', style: TextStyle(fontSize: 32))),
+    Center(child: Text('Pagina 3', style: TextStyle(fontSize: 32))),
+    Center(child: Text('Pagina 4', style: TextStyle(fontSize: 32))),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          buildContainer(
-            urlImage:
-                'https://www.envoyage.com/sites/default/files/styles/full_size/public/Dest_Netherlands_Landing.jpg?itok=AXtp56Mm',
-            local: 'Harlingen, Netherlands',
-            dates: '18 - 23 Dec',
-            host: 'Professional Host',
-            total: '\$1,068 total',
-          ),
-          buildContainer(
-            urlImage:
-                'https://blog.theifriend.com/wp-content/uploads/2021/11/guia-de-viagem-maragogi-al-piscinas-naturais-990x556.png',
-            local: 'Maragogi, Brazil',
-            host: 'Professional Host',
-            dates: '10 Jan - 31 Dec',
-            total: '\$5,200 total',
-          ),
+      body: pages[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Color(0xFFE41D56),
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Explore'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Wishlists'),
+          BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.airbnb), label: 'Trips'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Inbox'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
       ),
     );
-  }
-
-  buildContainer({
-    required String urlImage,
-    required String local,
-    required String host,
-    required String dates,
-    required String total,
-  }) {
-    return Container(
-      margin: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              ClipRRect(
-                child: Image.network(urlImage, height: 300, fit: BoxFit.cover),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              Positioned(
-                child: Icon(Icons.favorite, size: 36, color: Colors.pinkAccent),
-                top: 16,
-                right: 16,
-              ),
-            ],
-          ),
-
-          SizedBox(height: 8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                local,
-                style: GoogleFonts.inter(fontWeight: FontWeight.bold, fontSize: 16), //TextStyle(),
-              ),
-              Row(children: [Icon(Icons.star, size: 16), buildText('4.76')]),
-            ],
-          ),
-          buildText(host),
-          buildText(dates),
-          buildText(total),
-        ],
-      ),
-    );
-  }
-
-  buildText(String text) {
-    return Text(text, style: GoogleFonts.inter());
   }
 }
