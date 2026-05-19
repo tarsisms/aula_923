277dbd882dfd6d09fd3dd2fedf5fd83a18e2b589
tarsis <tarsis.souza@ifal.edu.br>
2026-05-19 16:45:46 -0300

    Aula 06 - ListView.builder - 19/05

---
+++ lib/domain/propriedade.dart
@@ -0,0 +1,22 @@
+class Propriedade {
+  String urlImage;
+  String local;
+  String dates;
+  String host;
+  String total;
+
+  Propriedade({
+    required this.urlImage,
+    required this.local,
+    required this.dates,
+    required this.host,
+    required this.total,
+  });
+
+  // urlImage:
+  // 'https://www.envoyage.com/sites/default/files/styles/full_size/public/Dest_Netherlands_Landing.jpg?itok=AXtp56Mm',
+  // local: 'Harlingen, Netherlands',
+  // dates: '18 - 23 Dec',
+  // host: 'Professional Host',
+  // total: '\$1,068 total',
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:aula_923/explore_page.dart';
+import 'package:aula_923/pages/explore_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
